ea78dc2b1ddfd704ab0c1c7b8ba54706e9af12ed
gabgabze <denzrich10@gmail.com>
2025-06-29 16:04:29 +0300

    modified the script to provide db configs

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,25 +1,30 @@
 from os import environ
-import mysql.connector
+
+import mysql
 from mysql.connector import connect,Error
 
 # connection details
 try:
-    with connect(
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        passwd="Chelsea@123",
+        database="alx_book_store"
+    )
+    """with connect(
             host='127.0.0.1',
             port=3306,
             user='root',
             password='Chelsea@123',
             database="alx_book_store"
-    ) as con:
-        cur = con.cursor()
-        # create db if it does not exist
-        create_db = f"CREATE DATABASE IF NOT EXISTS alx_book_store;"
-        cur.execute(create_db)
-        print(f"Database 'alx_book_store' created successfully!")
+    ) as con:"""
+    mydb.cursor()
+    # create db
+    create_db = f"CREATE DATABASE IF NOT EXISTS alx_book_store"
+    #mydb.execute(create_db)
+    print("Database 'alx_book_store' created successfully!")
 except Error as e:
     print("Error while connecting to MySQL", e)
 
-
 # Close connections
-con.close()
-con.close()
+mydb.close()
