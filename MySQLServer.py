3576f3bb6ccc6c0eb3fa82341066e7b1f1f56d15
gabgabze <denzrich10@gmail.com>
2025-06-29 15:23:57 +0300

    renamed the corrupt file

--- MySQLServer.py
+++ MySQLServer.py
@@ -7,10 +7,8 @@ try:
     with connect(
             host='127.0.0.1',
             port=3306,
-            #user= environ.get('DB_USER'),
             user='root',
             password='Chelsea@123',
-            #password= environ.get('DB_PASSWORD'),
             database="alx_book_store"
     ) as con:
         print("Database 'alx_book_store' created successfully!")
