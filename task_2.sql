7bf14ee29d7d9df4e661902ce5442297dd95a0a7
gabgabze <denzrich10@gmail.com>
2025-06-29 16:52:34 +0300

    created the db attributes

---
+++ task_2.sql
@@ -0,0 +1,46 @@
+-- create db
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+-- switch to the database
+USE alx_book_store;
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    price DOUBLE,
+    publication_date DATE,
+    author_id int not null,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+);
+
+-- create Author table
+CREATE TABLE Authors(
+author_id INT PRIMARY KEY,
+author_name VARCHAR(215)
+);
+
+-- Create Customer table
+CREATE TABLE Customers(
+customer_id INT PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+-- create orders
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE,
+    customer_id int not null
+);
+
+-- create order details
+CREATE TABLE Order_Details(
+    order_detail_id INT PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE,
+    order_id int not null,
+    book_id int not null
+);
